feaac39a660ace5c49ace990bbd37ae3556d1e13
Vu Vo <vohoanvu96@gmail.com>
2023-08-14 21:26:59 +0700

    fixed linux errors

--- db/schema.rb
+++ db/schema.rb
@@ -1,30 +1,29 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20110613204114) do
+ActiveRecord::Schema.define(version: 2011_06_13_204114) do
 
   create_table "lists", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string   "name"
-    t.boolean  "done"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "list_id"
+    t.string "name"
+    t.boolean "done"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "list_id"
   end
 
 end
